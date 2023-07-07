290af1a932cf5196171017b523469a69f51efa89
AweSkyBear <>
2023-07-07 22:06:34 +0300

    feat: allow the path functions to receive additional params
    Always receive the choice and the ...rest args if passed (as an array)

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 export const basedOn =
-  <T extends string | number | symbol>(paths: Record<T, () => void>) =>
-  (choice: T) => {
-    return paths[choice]();
+  <T extends string | number | symbol, P extends any[] = void[]>(
+    paths: Record<T, (...args: [T, ...P]) => void>
+  ) =>
+  (choice: T, ...rest: P) => {
+    return paths[choice](choice, ...rest);
   };
 
 export const switchOn = basedOn;
 export const basedOnPartial =
-  <T extends string | number | symbol>(paths: Partial<Record<T, () => void>>) =>
-  (choice: T) => {
-    return paths[choice] && paths[choice]();
+  <T extends string | number | symbol, P extends any[] = void[]>(
+    paths: Partial<Record<T, (...args: [T, ...P]) => void>>
+  ) =>
+  (choice: T, ...rest: P) => {
+    return paths[choice] && paths[choice](choice, ...rest);
   };
 export const switchOnPartial = basedOnPartial;
